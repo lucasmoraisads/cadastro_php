e7b519455d1ef9a50b3c9da52693561cda5a0f89
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-07-22 07:18:54 -0300

    home

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
@@ -7,7 +7,29 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" >
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cadastro</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="novo_usuario.php">Novo Usuario</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="listar_usuario.php">Listar Usuarios</a>
+                </li>
+                
+            </ul>
+            </div>
+        </div>
+    </nav>
     <script src="js/bootstrap.bundle.min.js" ></script>
   </body>
 </html>
